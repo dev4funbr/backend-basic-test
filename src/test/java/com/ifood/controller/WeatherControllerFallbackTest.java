01203ae05f6731b6e72a0182b6a314f0a7e18aa9
dev4fubr <eduardo.km@gmail.com>
2020-10-22 11:23:39 -0300

    weather service fallback test

--- src/test/java/com/ifood/controller/WeatherControllerFallbackTest.java
+++ src/test/java/com/ifood/controller/WeatherControllerFallbackTest.java
@@ -1,20 +1,16 @@
 package com.ifood.controller;
 
-import com.ifood.client.OpenWeatherMapClient;
-import com.ifood.client.WeatherInformationResponse;
-import com.ifood.util.WeatherInformationResponseMock;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,7 +19,9 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class WeatherControllerTest {
+@TestPropertySource(properties = "client.openweathermap.key=123")
+
+public class WeatherControllerFallbackTest {
 
     @Autowired
     private WebApplicationContext wac;
@@ -33,27 +31,17 @@ public class WeatherControllerTest {
     @Autowired
     private WeatherController weatherController;
 
-    @MockBean
-    private OpenWeatherMapClient openWeatherMapClient;
-
     @Before
     public void setup() {
         this.mockMvc = standaloneSetup(this.weatherController).build();
     }
 
     @Test
-    public void givenCity_whenConsultWeatherService_thenReturnWeatherInformation() throws Exception {
-        final WeatherInformationResponse weatherInformationResponse = WeatherInformationResponseMock.newMock();
+    public void givenCity_whenWeatherServiceIsDown_thenReturnError() throws Exception {
         final String cityName = "London";
-        when(openWeatherMapClient.getWeatherInfo(cityName, "apptestekey", "metric")).thenReturn(weatherInformationResponse);
         MvcResult mvcResult = this.mockMvc.perform(get("/weather?cityName=" + cityName))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.description").value(weatherInformationResponse.getWeather().get(0).getDescription()))
-                .andExpect(jsonPath("$.humidity").value(weatherInformationResponse.getMain().getHumidity()))
-                .andExpect(jsonPath("$.temperature").value(weatherInformationResponse.getMain().getTemperature()))
-                .andExpect(jsonPath("$.maxTemperature").value(weatherInformationResponse.getMain().getMaximumTemperature()))
-                .andExpect(jsonPath("$.minTemperature").value(weatherInformationResponse.getMain().getMinimumTemperature()))
-                .andExpect(jsonPath("$.pressure").value(weatherInformationResponse.getMain().getPressure()))
+                .andExpect(jsonPath("$.description").value("Error: Weather Service Unavailable"))
                 .andReturn();
 
     }
